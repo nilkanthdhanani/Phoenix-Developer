8ed97ef8e0ed241f0de1d7c94f9edd02cf70eda7
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-07-09 16:23:16 +0530

    header color

--- src/App.js
+++ src/App.js
@@ -2,19 +2,8 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './routes';
 import './styles/index.scss';
-import Aos from 'aos';
-import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    Aos.init({
-      duration: 1000, // Animation duration
-      once: false,    // Animations should happen multiple times
-    });
-    return () => {
-      Aos.refresh(); // Ensure animations refresh on component unmount
-    };
-  }, []);
   return (
     <>
       <RouterProvider router={router} />
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeroBanner from './heroBanner';
 import Design from './design';
 import Lady from './lady';
@@ -6,8 +6,19 @@ import Digital from './digital';
 import WhatWeDo from './whatWeDo';
 import Studio from './studio';
 import Form from './form';
+import Aos from 'aos';
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000, // Animation duration
+      once: false,    // Animations should happen multiple times
+    });
+    return () => {
+      Aos.refresh(); // Ensure animations refresh on component unmount
+    };
+  }, []);
+
   return (
     <div>
       <HeroBanner/>
